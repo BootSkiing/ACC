d1b169e25e36a49e3667c9552a1166a4b7a9fbd9
chj8702 <chj8702@rit.edu>
2019-02-16 15:29:58 -0500

    Displays main page

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 """
 Now configure with Flask! Running this will display a web API on a local port
 """
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    print('Welcome to Awesome project')
-    return "Welcome to Awesome project"
+    return render_template("mainpage.html")
+
+if __name__ == '__main__':
+    app.run(debug=False)
