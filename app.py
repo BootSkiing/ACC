878ec420f2aa0240645b28561f702286f3f2fa6a
chj8702 <chj8702@rit.edu>
2019-02-16 14:10:57 -0500

    Added Flask to app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('Welcome to Awesome project')
+"""
+Now configure with Flask! Running this will display a web API on a local port
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    print('Welcome to Awesome project')
+    return "Welcome to Awesome project"
