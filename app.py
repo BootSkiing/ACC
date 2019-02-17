ce25798f6705840391bd059fcba5bff7c2df88ce
chj8702 <chj8702@rit.edu>
2019-02-16 23:37:04 -0500

    Templates for desc_page, submit, and results

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-Now configure with Flask! Running this will display a web API on a local port
+Now configured with Flask! Running this will display a web API on a local port
 """
 from flask import Flask, render_template, request, flash, send_from_directory
 from flask_bootstrap import Bootstrap
@@ -18,6 +18,19 @@ def main():
 def send_image(filename):
     return send_from_directory("./images", filename)
 
+@app.route("/<filename>")
+def desc_page(filename):
+    file = filename[-4:]
+    return render_template("desc_page_template", file = file, filename = filename)
+
+@app.route("/sumbit")
+def submit():
+    return render_template("submit")
+
+@app.route("/result")
+# Needs to be given data from the Neural Net
+def result(data):
+    return render_template()
 
 if __name__ == '__main__':
     app.run(debug=True)
