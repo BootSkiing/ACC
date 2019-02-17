6d4ea858e5cc0c2ed999c83ec2f89120b51d8e57
chj8702 <chj8702@rit.edu>
2019-02-16 21:49:17 -0500

    Images in folder are now displayed on homepage

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 """
 Now configure with Flask! Running this will display a web API on a local port
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, send_from_directory
+from flask_bootstrap import Bootstrap
+import os
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 @app.route("/")
 def main():
-    return render_template("mainpage.html")
+    image_names = os.listdir("./images")
+    return render_template("mainpage.html", image_names = image_names)
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("./images", filename)
+
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
