660d166920320699ede35c8d38450baa17bea853
Ayobami <aadewale123@gmail.com>
2019-02-17 08:34:37 -0500

    Updated database.py

--- src/database.py
+++ src/database.py
@@ -1,47 +1,49 @@
 
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+
 
 def create_arr(answers,data,rows,columns):
     gnb=GaussianNB()
     person=[]
     y=[]
-    for i in range(rows):
-        for val in range (columns-1):
-            if(val!="Serious Mental Illness"):
-                person.append(data.iat[val,i])
-        answers.append(person)
-        person=[]
-    for val in (data["Serious Mental Illness"]):
-        y.append(val)
-    print(answers[0])
-    print(len(answers))
-    training_number=int(len(answers)*0.75)
-    testing_number=int(len(answers)*0.25)
-    print()
-    x_train=answers[:training_number]
-    y_train=y[:training_number]
-    x_test=answers[training_number:]
-    y_test=y[training_number:]
-    for test in range (testing_number):
-        gnb.fit([x_train[test]],[y_train[test]])
+    
+    feature_cols = list(data.columns[1:60])
+    target_col = data.columns[60]
+
+    # Separate the data into feature data and target data (X_all and y_all, respectively)
+    X_all = data[feature_cols]
+    y_all = data[target_col]
+    num_all = data.shape[0] 
+    num_train = int(num_all*0.75) # about 75% of the data
+    num_test = num_all - num_train
+
+    # begin splitting data into various sets for comparision
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_all, y_all, test_size=num_test, random_state=5)
+    print("Shuffling of data into test and training sets complete!")
+    print("Training set: {} samples".format(X_train.shape[0]))
+    print("Test set: {} samples".format(X_test.shape[0]))    
+    gnb.fit(X_train, y_train)
+    results=(y_test==gnb.predict(X_test))
     ytrue=0
     yfalse=0
-    for test in range (testing_number):
-        prediction=gnb.predict([x_train[test]])==y_test[test]
-        if(prediction):
+    for result in results:
+        if(result):
             ytrue+=1
         else:
             yfalse+=1
-    
-    print("True:", ytrue)
-    print("False:", yfalse)
-     
+    print("True: ",ytrue)
+    print("False: ",yfalse)
+    print("Score:",ytrue/num_test)
+
+
 
     
 def main():
     #get the file
-    loc=r"num.csv"
+    loc=r"C:\Users\Devansh\Desktop\Projects\num.csv"
     data=pd.read_csv(loc)
     a=data.shape
     columns=a[0]
