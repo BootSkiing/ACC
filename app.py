55f86f14911cb1dffea584b66052b64297018f53
chj8702 <chj8702@rit.edu>
2019-02-17 02:41:09 -0500

    All templates have working Nav bars!

--- app.py
+++ app.py
@@ -5,32 +5,41 @@ from flask import Flask, render_template, request, flash, send_from_directory
 from flask_bootstrap import Bootstrap
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="./templates")
 bootstrap = Bootstrap(app)
 
 
 @app.route("/")
 def main():
     image_names = os.listdir("./images")
-    return render_template("mainpage.html", image_names = image_names)
+    return render_template("mainpage.html", image_names=image_names)
+
 
 @app.route('/upload/<filename>')
 def send_image(filename):
     return send_from_directory("./images", filename)
 
+
 @app.route("/<filename>")
 def desc_page(filename):
     file = filename[-4:]
-    return render_template("desc_page_template", file = file, filename = filename)
+    return render_template("desc_page_template.html", file=file, filename=filename)
 
-@app.route("/sumbit")
+
+@app.route("/submit")
 def submit():
-    return render_template("submit")
+    return render_template("submit.html")
 
-@app.route("/result")
+
+@app.route("/result", methods=['POST', 'GET'])
 # Needs to be given data from the Neural Net
-def result(data):
-    return render_template()
+def result():
+    if request.method == 'POST':
+        form = request.form
+        # This line will send form data to a seperate file to be processed. The returned value is the result
+        result = AI.process(form)
+        return render_template("result.html", result=result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
